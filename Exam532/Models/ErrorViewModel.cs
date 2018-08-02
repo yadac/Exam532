a6b591f248155466b022139f7b3a69c6367174c0
yosuke.adachi <yosuke.adachi@avanade.com>
2018-08-02 21:53:36 +0900

    Add project files.

---
+++ Exam532/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Exam532.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
